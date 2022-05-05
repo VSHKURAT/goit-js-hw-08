6db5068ce06eb0696dd699da9052c7ebe888da41
Volodymyr Shkuratovskyi <91778791+VSHKURAT@users.noreply.github.com>
2022-05-05 16:42:49 +0300

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const TIME_KEY = "videoplayer-current-time"
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('timeupdate', throttle(getTime,1000));
+    player.on('play', setTime);
+    
+    
+function getTime(){
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem(TIME_KEY, seconds)
+    })
+}
+
+function setTime(){
+    const savedTime = localStorage.getItem(TIME_KEY)
+    player.setCurrentTime(savedTime)
+    
+}
